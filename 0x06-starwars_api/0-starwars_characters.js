539569b1f04b0f0ec13fb04465d2f7f2b00fdb55
jedidia-nku <jedinkuze123@gmail.com>
2024-08-08 21:34:57 +0200

    Starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request')
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
